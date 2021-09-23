f77684767d5cbd0700b958775777c1367f634619
ikneel <buigigame@gmail.com>
2021-09-22 20:28:09 -0500

    salaverga si se pudo xd

--- hola.java
+++ hola.java
@@ -1,6 +1,6 @@
 public class hola{
          public static void main (String [] args){
-            System.out.println("Hola Mundo criko");
+            System.out.println("Hola Mundo crikoso");
 
          }
 }
